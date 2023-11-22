4c14a456c5bf284f68f36eddc4d9b28e71bff96e
Isabella Wartha <isabella.wartha@gmail.com>
2023-11-21 22:49:43 -0300

    ✨ feat: folders

---
+++ src/app/module/tasks/tasks.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CreateTaskComponent } from './components/create-task/create-task.component';
+
+
+
+@NgModule({
+  declarations: [
+    CreateTaskComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class TasksModule { }
